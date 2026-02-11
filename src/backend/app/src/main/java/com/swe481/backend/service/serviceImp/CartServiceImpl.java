adc3ce7118108d12bb60cbc9ff8ae9763764b967
Lobaali <lobaalih@gmail.com>
2026-02-11 20:02:57 +0300

    Model and Service Folders

---
+++ src/backend/app/src/main/java/com/swe481/backend/service/serviceImp/CartServiceImpl.java
@@ -0,0 +1,5 @@
+package com.swe481.backend.service.serviceImp;
+
+public class CartServiceImpl {
+    
+}
---
+++ src/backend/app/src/main/java/com/swe481/backend/service/serviceInterface/CartService.java
@@ -0,0 +1,5 @@
+package com.swe481.backend.service.serviceInterface;
+
+public class CartService {
+    
+}
