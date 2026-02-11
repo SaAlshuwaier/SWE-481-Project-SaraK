7d277c856feaaa3c1b8b0e202c79978b88cf0ea5
Reem K <159922917+re1k@users.noreply.github.com>
2026-02-11 22:52:36 +0300

    Lambok lib, Star and Genres Models (#25)
    * Add build job to validate database secrets
    
    Add a build job to check for database secrets in CI workflow.
    
    * add lambok, star and geners models
    
    ---------
    
    Co-authored-by: LayanBS7 <Layanbsa@hotmail.com>

---
+++ src/backend/app/src/main/java/com/swe481/backend/controller/StarController.java
@@ -0,0 +1,9 @@
+package com.swe481.backend.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/starts")
+public class StarController {
+
+}
