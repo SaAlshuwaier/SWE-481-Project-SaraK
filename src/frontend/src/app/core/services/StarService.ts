9ee4c497b105c036f1145d4cc6eb78578e56ad31
Reem K <159922917+re1k@users.noreply.github.com>
2026-02-14 22:24:22 +0300

    StarService Api FrontEnd (#38)
    * Add build job to validate database secrets
    
    Add a build job to check for database secrets in CI workflow.
    
    * Merge branch ':main' into Reem-personal-development (#2)
    
    * add lambok, star and geners models
    
    * Star & Genre Controllers and Service Interfaces
    
    * setting up Dto, frontEnd folder Structue
    
    * add html page
    
    * html page
    
    * Reem personal development (#4)
    
    * Add build job to validate database secrets
    
    Add a build job to check for database secrets in CI workflow.
    
    * Merge branch ':main' into Reem-personal-development (#2)
    
    * add lambok, star and geners models
    
    * Star & Genre Controllers and Service Interfaces
    
    * setting up Dto, frontEnd folder Structue
    
    * add html page
    
    * html page
    
    ---------
    
    Co-authored-by: LayanBS7 <Layanbsa@hotmail.com>
    
    * Front End Services(STAR.GENRE)
    
    * star api service front end
    
    ---------
    
    Co-authored-by: LayanBS7 <Layanbsa@hotmail.com>

--- src/frontend/src/app/core/services/StarService.ts
+++ src/frontend/src/app/core/services/StarService.ts
@@ -3,17 +3,26 @@ import {environment} from '../../../environment/environment';
 import {StarDto} from '../models/StarDto';
 import {Observable} from 'rxjs';
 import {Inject, Injectable} from '@angular/core';
+import {MovieDto} from '../models/MovieDto';
 
 @Injectable({
     providedIn:'root',
   })
 
 export class StarService {
-  private http = Inject(HttpClient);
+  constructor(private http: HttpClient) {}
   private baseUrl = environment.backendUrl;
 
-  getStar(starId: String) : Observable<StarDto> {
-    return this.http.get(`${this.baseUrl}/stars/${starId}`);
+  getStar(starId: string) : Observable<StarDto> {
+    return this.http.get<StarDto>(`${this.baseUrl}/api/stars/${starId}`);
+  }
+
+  getStarsOfMovie(movieId: string) : Observable<StarDto[]> {
+    return this.http.get<StarDto[]> (`${this.baseUrl}/api/movies/${movieId}/stars`);
+  }
+
+  getMoviesOfStar(starId: string) : Observable<MovieDto[]> {
+    return this.http.get<MovieDto[]>(`${this.baseUrl}/api/stars/${starId}/movies`);
   }
 
 }
---
+++ src/frontend/src/app/features/FUTURE.ts
@@ -0,0 +1,2 @@
+//for upcoming Phases we Put folders naming then
+// the features and in there the ts components will give us the Services
