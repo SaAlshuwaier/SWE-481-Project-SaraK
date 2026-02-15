511e2c680e3a09bb08c642819e7901959771dad6
Lobaali <lobaalih@gmail.com>
2026-02-15 22:09:17 +0300

    Frontend and backend contract

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -126,7 +126,7 @@ export class App {
     });
   }
 
-  // POST /api/cart/updateItem/{movieId}
+  // PATCH /api/cart/updateItem/{movieId}
   cartUpdate() {
     this.cartResult.set(null);
     this.cartError.set(null);
