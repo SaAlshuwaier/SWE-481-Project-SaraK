95925e12b03a7a9bbca1ab77596b6f41259d81fc
Janau00 <jana.abdullah.u00@gmail.com>
2026-02-16 01:12:47 +0300

    frontend and backend contract (checkout and auth)

--- src/backend/app/src/main/java/com/swe481/backend/model/Auth/LoginResponse.java
+++ src/backend/app/src/main/java/com/swe481/backend/model/Auth/LoginResponse.java
@@ -3,14 +3,20 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+/**
+ * LoginResponse represents the response returned after login attempt.
+ *
+ * Example:
+ * {
+ *   "message": "Login successful",
+ *   "success": true
+ * }
+ */
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LoginResponse {
-        // What backend returns to frontend:
-    // { "message": "Login successful", "success": true }
     private String message;
     private boolean success;
 }
--- src/backend/app/src/main/java/com/swe481/backend/model/Auth/LogoutResponse.java
+++ src/backend/app/src/main/java/com/swe481/backend/model/Auth/LogoutResponse.java
@@ -3,7 +3,15 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+/**
+ * LogoutResponse represents the response returned after logout.
+ *
+ * Example JSON:
+ * {
+ *   "message": "Logout successful",
+ *   "success": true
+ * }
+ */
 @Getter
 @Setter
 @AllArgsConstructor
