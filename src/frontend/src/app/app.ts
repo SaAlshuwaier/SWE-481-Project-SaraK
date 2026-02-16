0c17118aa535d58f373a8485541540451749ee45
Reem K <159922917+re1k@users.noreply.github.com>
2026-02-16 06:59:07 +0300

    Remove unused Observable import from app.ts

--- src/frontend/src/app/app.ts
+++ src/frontend/src/app/app.ts
@@ -25,7 +25,7 @@ import { StarService } from './core/services/StarService';
 import { StarDto } from './core/models/StarDto';
 import { GenreService } from './core/services/GenreService';
 import { GenreDto } from './core/models/GenreDto';
-import {Observable} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
