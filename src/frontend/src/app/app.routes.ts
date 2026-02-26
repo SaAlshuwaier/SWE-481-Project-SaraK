2a11f3337d23495c9bcc98cd32918be01a04f1b5
re1k <reem.kd78@gmail.com>
2026-02-26 14:55:55 +0300

    frontend routes

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { BrowseMoviesComponent } from './pages/browseMovies/browseMovies.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  //path routes to pages are defined
+  {
+    path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
+  },
+  {
+    path: 'movies',
+    component: BrowseMoviesComponent
+  }
+];
