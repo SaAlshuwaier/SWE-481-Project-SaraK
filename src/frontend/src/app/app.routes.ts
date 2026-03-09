c2881272bb88f1cf1e7848f8d0294e7fcd6a82f8
Reem Khalid <159922917+re1k@users.noreply.github.com>
2026-03-09 07:48:07 +0300

    Remove route for starId in movies

--- src/frontend/src/app/app.routes.ts
+++ src/frontend/src/app/app.routes.ts
@@ -25,7 +25,6 @@ export const routes: Routes = [
   { path: 'movies', component: BrowseMoviesComponent },
   { path: 'movies/search', component: SearchMoviesComponent },
   { path: 'movies/genre/:genreId', component: BrowseMoviesComponent },
-  { path: 'movies/star/:starId', component: BrowseMoviesComponent },
   { path: 'movies/:movieId', component: MovieDetailsPageComponent },
 
   // stars
