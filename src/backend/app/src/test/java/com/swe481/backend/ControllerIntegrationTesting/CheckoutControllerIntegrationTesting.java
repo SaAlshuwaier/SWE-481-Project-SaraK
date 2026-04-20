91b8e7dec53f51fb766834f084c8bd436b27bbc2
Lobaali <lobaalih@gmail.com>
2026-04-20 19:31:12 +0300

    .

--- src/backend/app/src/test/java/com/swe481/backend/ControllerIntegrationTesting/CheckoutControllerIntegrationTesting.java
+++ src/backend/app/src/test/java/com/swe481/backend/ControllerIntegrationTesting/CheckoutControllerIntegrationTesting.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 
-@AutoConfigureMockMvc
+//@AutoConfigureMockMvc
 
 public class CheckoutControllerIntegrationTesting {
 
